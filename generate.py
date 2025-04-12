a72906e68124ca2a90efc1364e06bdbe21b33923
D V Sarayu Reddy <75120338+Sarayu4@users.noreply.github.com>
2025-04-12 11:57:39 +0530

    Delete generate.py

--- generate.py
+++
@@ -1,8 +0,0 @@
-import secrets
-
-def generate_key():
-    key = secrets.token_hex(32)  # Generates a 64-character secure key
-    print(f"Generated SECRET_KEY: {key}")
-
-if __name__ == "__main__":
-    generate_key()
